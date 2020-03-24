84128b8bd0e6d84bce625e5a7b4c296b318ffaa3
Warren Campbell <wcampbell@ara.com>
2020-03-23 21:39:47 -0400

    Updates source to build using MSVC 16 and ninja

--- main.cpp
+++ main.cpp
@@ -4,27 +4,51 @@
  * Source: https://raymii.org
  * Author: Remy van Elst, 2019
  */
-#define WINVER 0x0500
+#define WINVER 0x0A00
+#define _WIN32_WINNT 0x0A00
 #include <windows.h>
+
+#include <csignal>
+#include <atomic>
+#include <iostream>
+#include <thread>
+
+constexpr auto sleep_seconds = 5;
+
+std::atomic_bool stopRunning = false;
+void signal_handler(int signal)
+{
+  stopRunning = true;
+}
+
+void sendKey(boolean release = false)
+{
+  std::cout << "Sending F24 key ";
+  INPUT ip;
+  ip.type = INPUT_KEYBOARD;
+  ip.ki.wVk = 0x87; // A virtual-key code. 0x87 is the "F24" key.
+  ip.ki.wScan = 0;  // A hardware scan code for key.
+  if (release) {
+    std::cout << "up";
+    ip.ki.dwFlags = KEYEVENTF_KEYUP; // The key is being released.
+  } else {
+    ip.ki.dwFlags = 0; // If not specified, the key is being pressed. 
+    std::cout << "down";
+  }
+  ip.ki.time = 0; // The time stamp for the event, in milliseconds.
+  ip.ki.dwExtraInfo = 0; // An additional value associated with the keystroke.
+  std::cout << std::endl;
+  SendInput(1, &ip, sizeof(INPUT));
+}
+
 int main()
 {
-    // 40 seconds
-    DWORD sleeptime = 40000;
-    INPUT ip;
-    ip.type = INPUT_KEYBOARD;
-    ip.ki.wScan = 0; // hardware scan code for key
-    ip.ki.time = 0;
-    ip.ki.dwExtraInfo = 0;
-    // list of keycodes:
-    // http://web.archive.org/web/20191221104344/https://docs.microsoft.com/en-us/windows/win32/inputdev/virtual-key-codes
-    ip.ki.wVk = 0x87; // virtual-key code for the "F24" key
-    while (true)
-    {
-        ip.ki.dwFlags = 0; // 0 for key press
-        SendInput(1, &ip, sizeof(INPUT));
-        ip.ki.dwFlags = KEYEVENTF_KEYUP; // KEYEVENTF_KEYUP for key release
-        SendInput(1, &ip, sizeof(INPUT));
-        Sleep(sleeptime);
-    }
-    return 0;
+  std::signal(SIGINT, signal_handler);
+  while (!stopRunning) {
+    sendKey();
+    sendKey(true);
+    std::cout << "Sleeping " << sleep_seconds << " seconds..." << std::endl;
+    std::this_thread::sleep_for(std::chrono::seconds(sleep_seconds));
+  }
+  return 0;
 }
